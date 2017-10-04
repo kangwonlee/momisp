db9107624b3b95a69b934afaaaf38326172c7500
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-10-05 04:03:54 +0900

    added one more script to be tested; specified "--ExecutePreprocessor.kernel_name=python3" # http://nbconvert.readthedocs.io/en/latest/execute_api.html

--- test_nb.py
+++ test_nb.py
@@ -7,12 +7,15 @@ import tempfile
 
 
 def _exec_notebook(path):
+    # http://nbconvert.readthedocs.io/en/latest/execute_api.html
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python3",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
 
 def test():
     _exec_notebook(os.path.join('Ch04', 'ex04.001.SFD.BMD.numerical.arrows.ipynb'))
+    _exec_notebook(os.path.join('Ch02', 'ex02.002.varying.width.ipynb'))
