083315e9e93ac767b960d87781a4ee0c28445db5
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-11-03 01:49:19 +0900

    added draw_w_view()

--- utils/draw_diagrams.py
+++ utils/draw_diagrams.py
@@ -73,3 +73,69 @@ def rect_section_c():
     plt.text(corner_ne[0] + overhang_x + 0.01, offset + center[1] - .01, 'c')
 
     show_diagram(ax1)
+
+
+def draw_w_view(L_m, points_list, reaction_list, v_load_list=[], dist_load_list=[]):
+    """
+
+    :param float L_m: length of the beam  
+    :param list(dict) points_list: [{'x_m':x, 'label': txt}] 
+    :param list(dict) reaction_list: [{'x_m':x}] 
+    :return: 
+    """
+
+    # input argument cleansing
+    L_m = float(L_m)
+
+    # Nick Charton, Drawing and Animating Shapes with Matplotlib, nickcharton.net, https://nickcharlton.net/posts/drawing-animating-shapes-matplotlib.html
+    fig, ax = plt.subplots()
+
+    h_beam_m = L_m / 20.0
+
+    beam_patch = plt.Rectangle((0, 0), L_m, h_beam_m)
+    ax.add_patch(beam_patch)
+
+    y_text = -1
+
+    for point_dict in points_list:
+        ax.text(float(point_dict['x_m']), y_text, point_dict['text'])
+
+    y_arrow = -0.5
+    # 반력
+    # Reaction force
+    for reaction_dict in reaction_list:
+        ax.arrow(float(reaction_dict['x_m']), y_arrow,
+                 0, abs(y_arrow) * 0.7, head_width=0.1, head_length=abs(y_arrow) * 0.2)
+
+    y_load = h_beam_m * 2
+    # 집중하중 P
+    # Concentrated shear force load P
+    for v_load_dict in v_load_list:
+        # arrow heading upward
+        if 0 < v_load_dict['sign']:
+            ax.arrow(float(v_load_dict['x_m']), h_beam_m + 0.1,
+                     0, abs(y_arrow) * 0.7, head_width=0.1, head_length=abs(y_arrow) * 0.2)
+        # arrow heading downward
+        elif 0 > v_load_dict['sign']:
+            ax.arrow(float(v_load_dict['x_m']), y_load,
+                     0, - abs(y_arrow) * 0.7, head_width=0.1, head_length=abs(y_arrow) * 0.2)
+
+    # 분포하중 w0
+    # Distributed load w0
+    for dist_load_dict in dist_load_list:
+        x_load_m_array = np.linspace(float(dist_load_dict['x_begin_m']),
+                                     float(dist_load_dict['x_end_m']), 10 + 1)
+        y_load_m_array = np.ones_like(x_load_m_array) * y_load
+        u_load_m_array = np.zeros_like(x_load_m_array)
+        v_load_m_array = np.ones_like(x_load_m_array) * (h_beam_m * -1)
+
+        ax.quiver(x_load_m_array, y_load_m_array,
+                  u_load_m_array, v_load_m_array)
+
+        ax.text((float(dist_load_dict['x_begin_m']) + float(dist_load_dict['x_end_m'])) * 0.5, y_load + 0.1,
+                dist_load_dict['text'], horizontalalignment='center')
+
+    ax.axis('equal')
+    # Joe Kington, Emmet B, et al,. How to remove frame from matplotlib?, StackOverflow.com, 2016 Sep 16, https://stackoverflow.com/questions/14908576/
+    ax.axis('off')
+    plt.show()
