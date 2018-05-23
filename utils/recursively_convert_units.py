bb466a177acc19f89e8bf5a94c49a5c55712eb60
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-24 07:32:46 +0900

    Added os_walk_if_not_ignore() : Run an os.walk() loop and yield if not is_ignore()
    To make the chapter loop simpler.

--- utils/recursively_convert_units.py
+++ utils/recursively_convert_units.py
@@ -15,6 +15,17 @@ def is_ignore(path):
     return result
 
 
+def os_walk_if_not_ignore(root):
+    """
+    Run an os.walk() loop and yield if not is_ignore()
+
+    root : a path string to a folder
+    """
+    for root_name, dir_list, filename_list in os.walk(root):
+        if not is_ignore(root_name):
+            yield root_name, dir_list, filename_list
+
+
 def is_ipynb(path):
     return '.ipynb' == os.path.splitext(path)[-1]
 
@@ -24,12 +35,13 @@ def main():
     # file processor
     fp = sc.IpynbUnitConverter(None)
 
-    for root_name, _, filename_list in os.walk(os.pardir):
-        if not is_ignore(root_name):
-            for filename in filename_list:
-                if is_ipynb(filename):
-                    full_path = os.path.join(root_name, filename)
-                    fp.process_nb_file(full_path, b_write_file=True)
+    # Chapter loop
+    for root_name, _, filename_list in os_walk_if_not_ignore(os.pardir):
+        # ipynb file loop
+        for filename in filename_list:
+            if is_ipynb(filename):
+                full_path = os.path.join(root_name, filename)
+                fp.process_nb_file(full_path, b_write_file=True)
 
 
 if __name__ == '__main__':
