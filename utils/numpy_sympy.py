7f4e5062f26815b942447acacab6eacb40095516
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-24 18:25:13 +0900

    Added chapter loop and ipynb file loop

--- utils/numpy_sympy.py
+++ utils/numpy_sympy.py
@@ -1,1 +1,43 @@
+import ast
+import os
+import subprocess
+import sys
+
+import nbformat
+
 import recursively_convert_units as rcu
+import nbutils.nb_file_util as fu
+
+
+def main(argv):
+
+    # no argument
+    if not argv:
+        root = os.pardir
+    else:
+        root = argv[0]
+
+    # Chapter folder
+    for root_dir, _, filename_list in rcu.os_walk_if_not_ignore(root):
+
+        # Notebook file loop
+        for ipynb_filename in filter(rcu.is_ipynb, filename_list):
+
+            ipynb_filename_name = os.path.splitext(ipynb_filename)[0]
+
+            py_filename_full_path = os.path.join(root_dir, ipynb_filename_name + '.py')
+
+            if os.path.exists(py_filename_full_path):
+                raise IOError('file %s already exists.' % py_filename_full_path)
+
+            # convert a .ipynb file into a .py file
+            conversion_cmd = ['jupyter', 'nbconvert', 
+                              '--to', 'python', 
+                              '--output-dir='+root_dir,
+                              os.path.join(root_dir, ipynb_filename)]
+
+            print(conversion_cmd)
+
+
+if "__main__" == __name__:
+    main(sys.argv[1:])
