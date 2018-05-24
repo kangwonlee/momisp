b98667465a37a17a0b7f3d3e1163de1ebdec27e5
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-24 18:49:32 +0900

    Added test_get_conversion_cmd_list()

--- utils/tests/test_numpy_sympy.py
+++ utils/tests/test_numpy_sympy.py
@@ -4,4 +4,17 @@ import sys
 
 sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
 
-import numpy_sympy
+import numpy_sympy as ns
+
+
+def test_get_conversion_cmd_list():
+    root_dir = os.getcwd()
+    ipynb_filename = 'none.ipynb'
+
+    result = ns.get_conversion_cmd_list(root_dir, ipynb_filename)
+
+    assert isinstance(result, list)
+    assert 'jupyter' in result
+    assert 'nbconvert' in result
+    assert 'python' in result
+    assert os.path.join(root_dir, ipynb_filename) in result
