7dee24f4b86c04d6fd0b753c0c07f0c919079931
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-24 18:30:57 +0900

    Refactored get_conversion_cmd_list() : to modularize

--- utils/numpy_sympy.py
+++ utils/numpy_sympy.py
@@ -31,12 +31,19 @@ def main(argv):
                 raise IOError('file %s already exists.' % py_filename_full_path)
 
             # convert a .ipynb file into a .py file
-            conversion_cmd = ['jupyter', 'nbconvert', 
-                              '--to', 'python', 
-                              '--output-dir='+root_dir,
-                              os.path.join(root_dir, ipynb_filename)]
+            conversion_cmd = get_conversion_cmd_list(root_dir, ipynb_filename)
 
             print(conversion_cmd)
+
+
+def get_conversion_cmd_list(root_dir, ipynb_filename):
+    # convert a .ipynb file into a .py file
+    conversion_cmd = ['jupyter', 'nbconvert', 
+                      '--to', 'python', 
+                      '--output-dir='+root_dir,
+                      os.path.join(root_dir, ipynb_filename)]
+
+    return conversion_cmd
 
 
 if "__main__" == __name__:
