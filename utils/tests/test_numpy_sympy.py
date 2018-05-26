1f870b4cbe968c8b18c7ca5dd5b6a7cb315640be
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-26 12:09:19 +0900

    Started def test_process_one_ipynb_file():
    The function under test and this test function are not finished yet. Verify later.

--- utils/tests/test_numpy_sympy.py
+++ utils/tests/test_numpy_sympy.py
@@ -18,3 +18,15 @@ def test_get_conversion_cmd_list():
     assert 'nbconvert' in result
     assert 'python' in result
     assert os.path.join(root_dir, ipynb_filename) in result
+
+
+def test_process_one_ipynb_file():
+    # ../../Ch?? relative to the location of the file instead of the test execution location
+    chapter_dir = os.path.abspath(os.path.join(os.path.split(__file__)[0], os.pardir, os.pardir, 'Ch04_SFD.BMD'))
+    filename = 'ex04.003.simply.supported_v.w.ipynb'
+
+    # Function under test
+    result = ns.process_one_ipynb_file(chapter_dir, filename)
+
+    # TODO : The function under test and this test function are not finished. Verify later.
+    assert result is None
