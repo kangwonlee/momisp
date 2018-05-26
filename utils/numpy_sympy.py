9c33617da71d10b24666c38791727a5a28f4107b
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-26 15:21:01 +0900

    process_one_ipynb_file() : use an intermediate dictionary import_as_dict to store module names and import names

--- utils/numpy_sympy.py
+++ utils/numpy_sympy.py
@@ -70,28 +70,30 @@ def process_one_ipynb_file(root_dir, ipynb_filename,):
     for import_dict in results_list:
         line_split = import_dict['line'].split()
 
+        import_as_dict = {}
         # import something as sth
         if 'as' in line_split:
             i = line_split.index('as')
 
             # something
             if 0 < (i - 1):
-                import_dict['module'] = line_split[i - 1]
+                import_as_dict['module'] = line_split[i - 1]
             else:
                 raise SystemError("The location of 'as' seems not normal in %r" % (import_dict['line']))
 
             # sth
             if len(line_split[i - 1]) > (i + 1):
-                import_dict['as'] = line_split[i + 1]
+                import_as_dict['as'] = line_split[i + 1]
             else:
                 raise SystemError("The location of 'as' seems not normal in %r" % (import_dict['line']))
 
         elif 'import' in line_split:
             i = line_split.index('import')
             if len(line_split[i - 1]) > (1 + i):
-                import_dict['as'] = import_dict['module'] = line_split[i + 1]
+                import_as_dict['as'] = import_as_dict['module'] = line_split[i + 1]
 
-        print(import_dict, i)
+        import_dict.update(import_as_dict)
+        print(import_dict['line'], import_as_dict)
     # end import line loop
 
     tear_down(py_filename_full_path)
