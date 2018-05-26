621e5ec6af68a90ee0894adfbe2d9b62e515fdba
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-26 19:33:44 +0900

    Added sort_cases.py : Sort possible test cases for get_module_usage()
    pyTest may parametrize tests using @pytest.mark.parameterize decorator.
    # https://docs.pytest.org/en/latest/parametrize.html#parametrize
    
    Hoping to choose unique cases, sorted possible test cases

---
+++ utils/tests/sort_cases.py
@@ -0,0 +1,67 @@
+cases = [('Ch02_Strain', 'ex02.002.numpy.varying.width.ipynb', {'numpy': True}),('Ch02_Strain', 'ex02.007.numpy.reinforced.concrete.column.ipynb', {'numpy': True, 'numpy.linalg': True}),
+('Ch02_Strain', 'ex02.011.eq.thermal.stress.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': False}),('Ch03_Torsion', 'ch03.003.thin.wall.tube.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': False}),
+('Ch03_Torsion', 'ex03.000.radian.degree.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),('Ch03_Torsion', 'ex03.001.torsional.displacement.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True}),
+('Ch03_Torsion', 'ex03.002.statically.indeterminate.ipynb', {'numpy': True, 'numpy.linalg': True, 'sympy': True, 'degrees': True}),('Ch03_Torsion', 'ex03.006.thin.wall.tube.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'degrees': True}),
+('Ch03_Torsion', 'ex03.007.thin.wall.tube.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True}),
+('Ch04_SFD.BMD', 'ch04.004.SFD.BMD.area.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True}),('Ch04_SFD.BMD', 'ex04.001.simply.supported_v_numerical.arrows.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch04_SFD.BMD', 'ex04.002.simply.supported_m.only.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch04_SFD.BMD', 'ex04.003.simply.supported_v.w.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch04_SFD.BMD', 'ex04.005.area_simple_v.m.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch04_SFD.BMD', 'ex04.006.area_simple_v.w.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch05_Stress.in.Beams', 'ch05.000.Second.Moment.Of.Inertia.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch05_Stress.in.Beams', 'ch05.002.BendingStress.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch05_Stress.in.Beams', 'ch05.004.ShearStressBeam.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting':True}),
+('Ch05_Stress.in.Beams', 'ex05.001.BendingStress.BMD_rect_simple_w.p.ipynb', {'numpy': True, 'numpy.linalg': False, 'numpy.matlib':True, 'sympy': False}),
+('Ch05_Stress.in.Beams', 'ex05.002.BendingStress.T.section_simple.overhang_w.p.ipynb', {'numpy': True, 'numpy.linalg': False, 'numpy.matlib': True, 'sympy': True}),
+('Ch05_Stress.in.Beams', 'ex05.003.BendingStress.rect.two.h_cantilever_v.ipynb', {'numpy': True, 'numpy.linalg': False, 'numpy.matlib': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch05_Stress.in.Beams', 'ex05.004.BendingStress.W200.section_cantilever_m.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch05_Stress.in.Beams', 'ex05.005.BendingStress.lightest.section_W_simple_v.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch05_Stress.in.Beams', 'ex05.006.ShearStress.rect.section_simple_w.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch05_Stress.in.Beams', 'ex05.007.ShearStress.H.beam.v.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch05_Stress.in.Beams', 'ex05.009.Bending.Shear.Stress_max.length_rect_simple_w.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch05_Stress.in.Beams', 'ex05.010.Bending.Shear.Stress_box.section_overhang_v.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False}),
+('Ch05_Stress.in.Beams', 'ex05.011.Shear.Bearing.Stress_girder.section_fastener.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch05_Stress.in.Beams', 'pr05.062.ShearStress.H.beam.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch06_Deflection', 'ch06.000.Dirac.delta_Step.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch06_Deflection', 'ch06.004.moment_area.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch06_Deflection', 'ex06.001.W200_cantilever_w.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch06_Deflection', 'ex06.002.Direct.Integration_simple_w.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch06_Deflection', 'ex06.005.bracket_EI_simple_v.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch06_Deflection', 'ex06.008.moment_area.cantilever.w.P.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch06_Deflection', 'ex06.011.Method.of.Superposition_simple_p.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch07_Stat.Indet', 'ex07.001.Double.Integral_fix_simple_w.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch07_Stat.Indet', 'ex07.003.Double.Integral_bracket_fix_simple_v.ipynb', {'numpy': True, 'numpy.linalg': True, 'sympy': False, 'sympy.plotting': False}),
+('Ch07_Stat.Indet', 'ex07.003.Superposition_bracket_fix_simple_v.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch07_Stat.Indet', 'ex07.004.Double.Integral_bracket_fix_fix_w.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch07_Stat.Indet', 'ex07.005.Moment.Area_fix_simple_w.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': True}),
+('Ch07_Stat.Indet', 'ex07.008.Superposition_fix_simple_w.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ch08.005.2D.Stress.Transform.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ch08.006.Mohrs.Circle.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ch08.009.Strain.Transform.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ch08.010.Strain.Rosette.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ex08.001.Pressure_vessle.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ex08.002.Offset_Tension.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ex08.004.Stress_Transform.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ex08.007.Mohr.Circle.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch08_Stress_Due.To_Combined.Loads', 'ex08.008.Abs.Max.Tau_3D.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch10_Column', 'ch10.002.Euler.Equation_for_a.Column.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch10_Column', 'ex10.001.Column.W.Section.ipynb', {'numpy': False, 'numpy.linalg': False, 'sympy': True, 'sympy.plotting': False}),
+('Ch12_SpecialTopic', 'ch12.004.Theories.of.Failure_b.Ductile.Material.ipynb', {'numpy': True, 'numpy.linalg': False, 'sympy': False, 'sympy.plotting': False}),]
+
+keys_set = set()
+for t in cases:
+    d = t[-1]
+    keys_set = keys_set.union(set(d.keys()))
+keys_list = list(keys_set)
+keys_list.sort()
+
+def key_fun(t):
+    d = t[-1]
+    result = tuple([int(d.get(key, -1)) for key in keys_list])
+    return result
+
+cases.sort(key=key_fun)
+
+import pprint
+
+pprint.pprint(cases)
