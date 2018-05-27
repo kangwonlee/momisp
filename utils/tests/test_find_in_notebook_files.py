183d5f0216d29bf3b73947999b89979244bc763b
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-28 03:01:28 +0900

    Added test_find_or_replace_notebook_file_regex()
    Test found
    Test find_or_replace_in_one_cell

--- utils/tests/test_find_in_notebook_files.py
+++ utils/tests/test_find_in_notebook_files.py
@@ -31,3 +31,24 @@ def test_gen_cells_in_ipynb():
             assert 'source' in cell
         else:
             raise ValueError('Unknown cell type:', cell['cell_type'])
+
+
+def test_find_or_replace_notebook_file_regex():
+    ch_par_dir = ri.get_chapter_par_dir()
+    ipynb_full_path = os.path.join(ch_par_dir, 'Ch04_SFD.BMD', 'ex04.003.numpy.simply.supported_v.w.ipynb')
+    
+    assert os.path.exists(ipynb_full_path)
+
+    # Class object under test
+    nb = ri.FindOrReplaceNotebookFileRegex(ipynb_full_path, r'^####\s+B-C\s+구간$', '#### B-C 구간<br>B-C span', b_replace=True, b_verbose=True, b_arm=False)
+
+    sample_dict = {'cell_type': 'markdown', 'metadata': {}, 'source': '#### B-C 구간'}
+    expected_dict = {'cell_type': 'markdown', 'metadata': {}, 'source': '#### B-C 구간<br>B-C span'}
+
+    # Method under test 01
+    assert nb.found(sample_dict)
+
+    # Method under test 02
+    nb.find_or_replace_in_one_cell(sample_dict)
+
+    assert expected_dict == sample_dict
