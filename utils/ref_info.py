36b670e7d87e256445ca00c864f64b5409eb120f
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-27 20:37:32 +0900

    ref_info.py : Restarting from scratch

--- utils/ref_info.py
+++ utils/ref_info.py
@@ -1,34 +1,38 @@
 import os
 
-import nbutils.nb_file_util as fu
+import nbformat
+
 import recursively_convert_units as rcu
 
 
-class RefCellCorrector(fu.CellProcessorBase):
-
-    def __init__(self, cell=None):
-        super(RefCellCorrector, self).__init__(cell=cell)
-
-    def process_cell(self):
-        if not self.is_code():
-            print(self)
-
-
-class ReferenceTypoCorrector(fu.FileProcessor):
-
-    def __init__(self, nb_filename, cell_processor=RefCellCorrector()):
-        super(ReferenceTypoCorrector, self).__init__(nb_filename=nb_filename, cell_processor=cell_processor)
-
-
 def main():
-    fp = ReferenceTypoCorrector(None)
+
+    help(nbformat)
 
     # Chapter loop
-    for root_name, _, filename_list in rcu.os_walk_if_not_ignore(os.pardir):
-        # Notebook file loop
-        for filename in filter(rcu.is_ipynb, filename_list):
-            full_path = os.path.join(root_name, filename)
-            fp.process_nb_file(full_path, b_write_file=True)
+    for chapter_path, ipynb_filename in rcu.gen_ipynb(get_chapter_par_dir()):
+        process_one_ipynb(chapter_path, ipynb_filename)
+
+
+def process_one_ipynb(chapter_path, ipynb_filename):
+    pass
+
+
+def gen_cells_in_ipynb(ipynb_filename):
+    with open(ipynb_filename, 'rb') as nb_file:
+        txt = nb_file.read()
+    
+    nb_node = nbformat.reads(txt.decode(), nbformat.NO_CONVERT)
+
+    for cell in nb_node.cells:
+        yield cell
+
+
+def get_chapter_par_dir():
+    """
+    Absolute path to the parent directory of chapter folders
+    """
+    return os.path.abspath(os.path.join(os.path.split(__file__)[0], os.pardir))
 
 
 if __name__ == '__main__':
