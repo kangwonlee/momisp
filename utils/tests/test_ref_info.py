f7cea8ac97fd8837687e5fb7597bb69fedc76a68
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-27 21:28:24 +0900

    gen_cells_in_ipynb() : yield nb_node along with cell
    To enable further control
    test_gen_cells_in_ipynb() : accommodates gen_cells_in_ipynb() returning nbnodes and cells

--- utils/tests/test_ref_info.py
+++ utils/tests/test_ref_info.py
@@ -2,6 +2,8 @@ import os
 import pytest
 import sys
 
+import nbformat
+
 
 sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
 
@@ -13,7 +15,8 @@ def test_gen_cells_in_ipynb():
 
     ch_par_dir = ri.get_chapter_par_dir()
     ipynb_full_path = os.path.join(ch_par_dir, 'Ch04_SFD.BMD', 'ex04.003.numpy.simply.supported_v.w.ipynb')
-    for cell in ri.gen_cells_in_ipynb(ipynb_full_path):
+    for nbnodes, cell in ri.gen_cells_in_ipynb(ipynb_full_path):
+        assert isinstance(nbnodes, nbformat.NotebookNode)
         assert isinstance(cell, dict)
         assert 'cell_type' in cell
         if 'code' == cell['cell_type']:
