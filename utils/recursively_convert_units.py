bdd04bbf9b5c07ff1b3328f2e43a84ab7647f731
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-09-25 23:14:11 +0900

    moved os_walk_if_not_ignore() from momisp to nbutils

--- utils/recursively_convert_units.py
+++ utils/recursively_convert_units.py
@@ -3,17 +3,6 @@ import nbutils.nb_file_util as nf
 import os
 
 
-def os_walk_if_not_ignore(root):
-    """
-    Run an os.walk() loop and yield if not is_ignore()
-
-    root : a path string to a folder
-    """
-    for root_name, dir_list, filename_list in os.walk(root):
-        if not nf.is_ignore(root_name):
-            yield root_name, dir_list, filename_list
-
-
 def gen_filename_ipynb(filename_list):
     """
     Generator for ipynb filenames in the filename_list
@@ -31,7 +20,7 @@ def gen_ipynb(root):
     root(==parent folder of chapter folders) -> chapter_path, ipynb_filename
     """
     # Chapter folder
-    for chapter_path, _, filename_list in os_walk_if_not_ignore(root):
+    for chapter_path, _, filename_list in nf.os_walk_if_not_ignore(root):
 
         # Notebook file loop
         for ipynb_filename in filter(nf.is_ipynb, filename_list):
@@ -44,7 +33,7 @@ def main():
     fp = sc.IpynbUnitConverter(None)
 
     # Chapter loop
-    for root_name, _, filename_list in os_walk_if_not_ignore(os.pardir):
+    for root_name, _, filename_list in nf.os_walk_if_not_ignore(os.pardir):
         # ipynb file loop
         for ipynb_filename in filter(nf.is_ipynb, filename_list):
             full_path = os.path.join(root_name, ipynb_filename)
