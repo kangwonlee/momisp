4b59af81c370c973ab49b967df2d1b8faf0c051e
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-29 23:58:08 +0900

    roll back to d5a89bb619

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -1,7 +1,7 @@
-. ./.github/workflows/set_os_env.sh
-
+echo "exporting a new path ======================="
+export PATH="$MINICONDA_PATH:$MINICONDA_SUB_PATH:$PATH"
 echo "init conda ================================="
-$CONDA/$CONDA_SCRIPT/conda init bash
+conda init bash
 echo "~/$BASHRC =================================="
 . ~/$BASHRC
 echo "hash -r ===================================="
@@ -10,10 +10,13 @@ echo "============================================"
 echo "CONDA_PYTHON = $CONDA_PYTHON ==============="
 echo "============================================"
 echo "checking python version ===================="
-$CONDA/python --version
+python --version
 echo "conda config --yes ========================="
-$CONDA/$CONDA_SCRIPT/conda config --set always_yes yes --set changeps1 no;
+conda config --set always_yes yes --set changeps1 no;
 echo "conda info -a =============================="
-$CONDA/$CONDA_SCRIPT/conda info -a
+conda info -a
 echo "create test-environment ===================="
-$CONDA/$CONDA_SCRIPT/conda env create -n test-environment -f ./tests/environment.${CONDA_PYTHON}.yml
+conda env create -n test-environment -f ./tests/environment.${CONDA_PYTHON}.yml
+echo "activate test-environment =================="
+conda activate test-environment
+conda list
