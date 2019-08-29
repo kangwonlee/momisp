2652b0f39e49b6f5c7ed361c1458fc46386553a1
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-29 20:50:23 +0900

    which pytest before pytest

--- .github/workflows/run_test.sh
+++ .github/workflows/run_test.sh
@@ -1,6 +1,8 @@
+which py.test
 if [[ "RUNNER_OS" !=  "macOS" ]]; then
     CONDA_ENV_ROOT=$CONDA
 else
     CONDA_ENV_ROOT=/Users/runner/.conda
 fi
+echo $CONDA_ENV_ROOT/envs/test-environment/$CONDA_SCRIPT/py.test
 $CONDA_ENV_ROOT/envs/test-environment/$CONDA_SCRIPT/py.test --numprocesses=auto tests/
