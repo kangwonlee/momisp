3d45605e10274349bf98af6d93efdd9dce0ebf6d
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-29 20:46:22 +0900

    CONDA_ENV_ROOT for OSX

--- .github/workflows/run_test.sh
+++ .github/workflows/run_test.sh
@@ -1,1 +1,6 @@
-$CONDA/envs/test-environment/$CONDA_SCRIPT/py.test --numprocesses=auto tests/
+if [[ "RUNNER_OS" !=  "macOS" ]]; then
+    CONDA_ENV_ROOT=$CONDA
+else
+    CONDA_ENV_ROOT=/Users/runner/.conda
+fi
+$CONDA_ENV_ROOT/envs/test-environment/$CONDA_SCRIPT/py.test --numprocesses=auto tests/
