553a4d24c9c8fb4351f722b6a3698f576470ea8c
KangWon LEE <kangwon.lee@tukorea.ac.kr>
2022-04-28 14:59:19 +0900

    check if sample files have cells

--- utils/tests/test_add_colab_main_buttons.py
+++ utils/tests/test_add_colab_main_buttons.py
@@ -20,15 +20,22 @@ import add_colab_main_buttons as acb
 
 class TestAddColabMainButtons(unittest.TestCase):
     def setUp(self) -> None:
-        self.no_button_filename = "test_add_colab_main_buttons_without_button.ipynb"
-        self.no_button_folder = os.path.abspath(os.path.dirname(__file__))
-        self.no_button_full_path = os.path.join(self.no_button_folder, self.no_button_filename)
+        self.without_button_filename = "test_add_colab_main_buttons_without_button.ipynb"
+        self.without_button_folder = os.path.abspath(os.path.dirname(__file__))
+        self.without_button_full_path = os.path.join(self.without_button_folder, self.without_button_filename)
+
+        self.with_button_filename = "test_add_colab_main_buttons_with_button.ipynb"
+        self.with_button_folder = os.path.abspath(os.path.dirname(__file__))
+        self.with_button_full_path = os.path.join(self.with_button_folder, self.with_button_filename)
 
         return super().setUp()
 
-    def test_temp_file(self):
-        nb = nbformat.read(self.no_button_full_path, nbformat.NO_CONVERT)
-        assert "cells" in nb
+    def test_temp_files_have_cells(self):
+        nb_without = nbformat.read(self.without_button_full_path, nbformat.NO_CONVERT)
+        assert "cells" in nb_without
+
+        nb_with = nbformat.read(self.with_button_full_path, nbformat.NO_CONVERT)
+        assert "cells" in nb_with
 
 
 if "__main__" == __name__:
